1fe73a2165e1d44e2fc7a73a2943fe037ddad12e
Vish-10 <vishva.sivasami10@outlook.com>
2024-10-15 08:51:13 +0530

    spring bean xml configuration

--- Spring101/src/main/java/org/example/Alien.java
+++ Spring101/src/main/java/org/example/Alien.java
@@ -1,8 +1,52 @@
 package org.example;
 
 public class Alien {
+    int age;
+    private int age1;
+    private Laptop lap;//alien shouldn't be dependent on laptop so therefore we make a new interface called computer that will be extended by laptop and something like desktop
+    //we can create a Computer object and later based upon the object value that com has the respective function will be called
+
+    private  Computer com;
+    public Alien(){
+        System.out.println("Alien constructor");
+    }
+
+    public int getAge1() {
+        return age1;
+    }
+
+    public void setAge1(int age1) {
+        System.out.println("setter called");
+        this.age1 = age1;
+    }
+
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
+    public Computer getCom() {
+        return com;
+    }
+
+    public void setCom(Computer com) {
+        this.com = com;
+    }
 
     public void code(){
         System.out.println("Coding");
+    }
+
+    public void code1(){
+        System.out.println("Coding 1 called");
+        lap.compile();
+    }
+
+    public void code2(){
+        System.out.println("Coding 2 called");
+        com.compile();
     }
 }
--- Spring101/src/main/java/org/example/App.java
+++ Spring101/src/main/java/org/example/App.java
@@ -14,10 +14,42 @@ public class App
         //to use application context in a maven project we have to add it in the dependencies since applicationcontext is a part of spring framework
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); //this is a xml
         //since we are using xml here we have to create a xml file
+        //additional information: objects are created at this place - this refers to the xml file and creates all the objects that are mentioned there
 
         Alien obj = (Alien) context.getBean("alien"); //getbean returns an object of type object so thats the reason we have to type cast it
         //usually the spring has a container called IoC Container that creates and holds the objects for us
         //but here we haven't mentioned anything about spring it is just a plain maven project
         obj.code();
+
+        Alien obj1 = (Alien) context.getBean("alien");
+        Alien obj2 = (Alien) context.getBean("alien");
+        /**
+         * Here we may think that obj1 and obj2 are two diff objects
+         * But they're the same objects
+         */
+        obj1.age = 21;
+        System.out.println(obj1.age);
+        System.out.println(obj2.age);
+
+        //by default the scope is singleton
+        //inorder to get a new object everytime getBean is called we have to change the scope to prototype(xml file)
+
+        //setter injection
+        Alien obj3 = (Alien) context.getBean("alien");
+        //obj3.setAge1(33); //this is how we usually set the values
+        //now we have to set the value using the property tag in the xml file
+        //spring uses the setter method to inject the value
+        System.out.println(obj3.getAge1());
+
+        //this is for testing laptop object
+        obj3.code1();
+
+        //this is for testing interfaces
+        obj3.code2();
+
+        Desktop desktop = (Desktop) context.getBean("desktop");//this is for testing lazy-init
+
+
+
     }
 }
