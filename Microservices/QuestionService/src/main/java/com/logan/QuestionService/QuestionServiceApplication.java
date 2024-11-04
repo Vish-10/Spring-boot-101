6a292941ab56bf283f0e74bdc0bc380aebb92e65
Vish-10 <vishva.sivasami10@outlook.com>
2024-11-04 07:52:53 +0530

    microservices updated

--- Microservices/QuestionService/src/main/java/com/logan/QuestionService/QuestionServiceApplication.java
+++ Microservices/QuestionService/src/main/java/com/logan/QuestionService/QuestionServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuestionServiceApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(QuestionServiceApplication.class, args);
 	}
 
